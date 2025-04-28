fc9ae84f42e7e04cf1d74a70b6c3bb1b26428ad5
himanshu03vsk <58275123+himanshu03vsk@users.noreply.github.com>
2025-04-27 20:52:48 -0500

    commit

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-// Serve static files from the 'dist' folder
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// For any other requests, serve the index.html from the 'dist' folder
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
